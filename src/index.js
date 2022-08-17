e1b871e87d1712ad89e2f50536fd63db794d9571
Vadick <vadick1@gmail.com>
2022-08-17 18:00:01 -0500

    modificaciones para el deploy

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import './index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
